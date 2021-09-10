bae9337ec0d47d25f725680c7adf087f58504dda
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2021-09-10 09:04:32 +0100

    merge-db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,6 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+//const config = parse(process.env.DATABASE_URL);
+const config = parse("postgres://ygtbdkhctuvzog:c19bf9a5ea8a2a0aa1e3bc9a4b6d640dffd0baa5bb5ceb77a28d71bcfb266ce1@ec2-23-20-70-32.compute-1.amazonaws.com:5432/dfo54hfd3odho");
 
 module.exports = ({ env }) => ({
   defaultConnection: "default",
